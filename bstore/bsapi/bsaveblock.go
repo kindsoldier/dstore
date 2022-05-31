e9b1bc122ebada102c431e23ef56fb2dd31c161e
Oleg Borodin <onborodin@gmail.com>
2022-05-31 15:53:49 +0200

    lowcase api files

--- bstore/bsapi/bsaveblock.go
+++ bstore/bsapi/bsaveblock.go
@@ -11,6 +11,8 @@ type SaveBlockParams struct {
     FileId      int64           `json:"fileId"`
     BatchId     int64           `json:"batchId"`
     BlockId     int64           `json:"blockId"`
+    BlockSize   int64           `json:"blockSize"`
+    DataSize    int64           `json:"dataSize"`
 }
 
 type SaveBlockResult struct {
